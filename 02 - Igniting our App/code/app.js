c613767562bc141fba4288ee2b3b1155b345dcd2
Abhishek <abhishekY495.sec@gmail.com>
2023-10-19 23:09:05 +0530

    Added: Ep 02 Code and Notes

---
+++ 02 - Igniting our App/code/app.js
@@ -0,0 +1,17 @@
+const myInfo = React.createElement("div", {}, [
+  React.createElement("h1", {}, "About Me"),
+  React.createElement("p", {}, "Hi, my name is Abhishek"),
+  React.createElement(
+    "p",
+    {},
+    "Check out my ",
+    React.createElement(
+      "a",
+      { href: "https://github.com/abhishekY495", target: "_blank" },
+      "Github."
+    )
+  ),
+]);
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(myInfo);
